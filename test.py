475cb891a846053494e14e6873b3dcc76bad0a90
clandero <clandero@udec.cl>
2020-03-20 19:39:40 -0300

    auto-detect ip address

--- test.py
+++ test.py
@@ -30,5 +30,17 @@ def sequence(n):
     }
     return template('template.tpl', res)
     
-        
-run(host='localhost', port=8080, debug=True)
+import socket
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+run(host=str(get_ip()), port=8080, debug=True)
